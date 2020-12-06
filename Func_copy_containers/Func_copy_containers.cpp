1dba67a300485e02c4bb27d14e76221d08c05935
Farid <fabdullayev902@gmail.com>
2020-12-06 11:49:46 +0400

    first commit

---
+++ Func_copy_containers/Func_copy_containers.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <list>
+#include <vector>
+#include <set>
+
+/*
+		1. Реализуйте функцию которая копирует элементы из одного контейнера
+		   в другой но наоборот
+		2. В качестве примера можно скопировать элементы из std::set в
+		   std::vector
+*/
+
+template<typename T1, typename T2>
+void form_set_to_vector(const T1 from_copy, T2& to_copy)
+{
+
+	auto begin_from_copy = from_copy.rbegin();
+	auto end_from_copy = from_copy.rend();
+
+	to_copy.resize(from_copy.size());
+
+	auto begin_to_copy = to_copy.begin();
+
+	while (begin_from_copy != end_from_copy)  *begin_to_copy++ = *begin_from_copy++;
+
+}
+
+int main()
+{
+	std::list<double> arr;
+	std::set   <double> b;
+
+	b.insert(1.4);
+	b.insert(2.3);
+	b.insert(3.1);
+	b.insert(4.5);
+
+	form_set_to_vector(b, arr);
+
+	for (auto& item : arr)
+	{
+		std::cout << item << " ";
+	}
+}
